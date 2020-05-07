f3baef00cc381e45db235e632dd1af17c6668964
Jiejie Wang <jiejiewangzju@gmail.com>
2020-05-07 12:35:47 -0700

    docs:Add comment to bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+# Only explore data from the three cities above
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
